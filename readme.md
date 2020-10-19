23203a3ebe2438db6a635cbd35abd29b505917bf
Pranay <pranay.rungta@gmail.com>
2020-10-19 13:00:32 +0530

    added readme and setup.py updated

---
+++ readme.md
@@ -0,0 +1,10 @@
+Quick start
+===========
+
+```
+git clone https://github.com/pranayrungta/apple.git
+
+cd path/to/repository
+
+pip install -e .
+```
--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
 setup( name='apl',
-       packages=['apl']
+       packages=['apl'],
+       author='Pranay Rungta',
+       author_email='pranay.rungta@gmail.com',
+       install_requires=['pandas', 'sklearn']
        )
